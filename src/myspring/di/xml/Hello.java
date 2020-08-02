7bcbcc29918a3d8fa8a09a44049710dd40196d79
jay choi <jay.choi@allwon21.com>
2020-08-02 18:11:04 +0900

    JUnit test 2 // list, set, maps

--- src/myspring/di/xml/Hello.java
+++ src/myspring/di/xml/Hello.java
@@ -1,10 +1,38 @@
 package myspring.di.xml;
 
+import java.util.List;
+
 //POJO
 public class Hello {
 	private String name;
 	private Printer printer;
+	private List<String> names;
 	
+	
+	public Hello() {
+		
+	}
+			
+	
+	
+	public List<String> getNames() {
+		return names;
+	}
+
+
+
+	public void setNames(List<String> names) {
+		this.names = names;
+	}
+
+
+
+	public Hello(String name, Printer printer) {
+		super();
+		this.name = name;
+		this.printer = printer;
+	}
+
 	//setter method로 di하기
 	
 	//setname 쓰고  ctrl+space
--- src/myspring/di/xml/test/HelloBeanJUnitSpringTest.java
+++ src/myspring/di/xml/test/HelloBeanJUnitSpringTest.java
@@ -1,7 +1,11 @@
 package myspring.di.xml.test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 
+import java.util.List;
+
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,27 +14,49 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import myspring.di.xml.Hello;
+import myspring.di.xml.Printer;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations="classpath:config/beans.xml")
+@ContextConfiguration(locations = "classpath:config/beans.xml")
+
 public class HelloBeanJUnitSpringTest {
-	
-	//bean 주입
+
+	// bean 주입
 	@Autowired
-	ApplicationContext context;	
-	
-	@Test
-	public void test() {
-		//2. getBean() 호출
-		Hello hello = (Hello)context.getBean("hello");
-		
-		Hello hello2 = (Hello)context.getBean("hello");			
-		
-		//hello와 hello2의 레퍼(주소)런스가 같은지 확인
-		assertSame(hello, hello2); // == true
-		
-		//스프링 컨테이너는 bean을 싱글톤(두번생성되지 않는다)으로 관리한다는 사실을 알 수 있음
-		
+	ApplicationContext context;
+
+	@Test @Ignore
+	public void test2() {
+		// 2. getBean() 호출
+		Hello hello = (Hello) context.getBean("hello");
+
+		Hello hello2 = (Hello) context.getBean("hello");
+
+		assertSame(hello, hello2);
+
+		// 스프링 컨테이너는 bean을 싱글톤(두번생성되지 않는다)으로 관리한다는 사실을 알 수 있음
+
 	}
+
+	@Test
+	public void test1() {
 		
+		Hello hello = (Hello) context.getBean("hello2"); //id값
+
+		assertEquals("Hello Spring", hello.sayHello());
+		
+		hello.print();
+		
+		assertEquals(3, hello.getNames().size());
+		List<String> list = hello.getNames();
+
+		for (String value : list) {
+			System.out.println(value);
+		}
+
+		// StringPrinter getBean()
+		Printer printer = context.getBean("printer", Printer.class);
+		assertEquals("Hello Spring", printer.toString());
+
+	}
 }
